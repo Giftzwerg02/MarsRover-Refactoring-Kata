322bb5f404a4db9997520276cda139bb7d54a83e
Unknown <chessplayer1@gmx.at>
2020-06-14 19:41:01 +0200

    Added two Tests-Chunks (see description):
    ### Multiple Rovers-Test (driveMultipleRoversTest):
    - Tests if multiple rovers can be driven independent from one-another
    
    ### Directional Movement-Test (drive<Direction>Test):
    - Tests if movement per direction is as expected

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -35,18 +35,42 @@ class MarsRoverTest {
 
     // region Benjamin-Tests
     @Test
-    public void generateMultipleRoversTest() {
-        MarsRover mr1 = new MarsRover();
-        MarsRover mr2 = new MarsRover();
-        assertNotEquals(mr1, mr2);
+    public void driveMultipleRoversTest() {
+        /*
+        * If a rover has finished moving, then the next rover should have an "independent" surrounding
+        * --> when given the same call twice (aka. two rovers drive the same path) then the output should be equal
+        * as well
+        * */
+
+        String roverPos1 = MarsRover.move(1, 2, 'N', "LMLMLMLMM");
+        String roverPos2 = MarsRover.move(1, 2, 'N', "LMLMLMLMM");
+        assertEquals(roverPos1, roverPos2);
     }
 
     @Test
-    public void startRoverTest() {
-        String newPosition = "";
-        newPosition = MarsRover.move(1,2, 'N', "5 5");
-        assertEquals("1 2 N", newPosition);
+    public void driveNorthTest() {
+        String northPos = MarsRover.move(1,1, 'N', "M");
+        assertEquals(northPos, "1 2 N");
     }
+
+    @Test
+    public void driveEastTest() {
+        String northPos = MarsRover.move(1,1, 'E', "M");
+        assertEquals(northPos, "2 1 E");
+    }
+
+    @Test
+    public void driveSouthTest() {
+        String northPos = MarsRover.move(1,1, 'S', "M");
+        assertEquals(northPos, "1 0 S");
+    }
+
+    @Test
+    public void driveWestTest() {
+        String northPos = MarsRover.move(1,1, 'W', "M");
+        assertEquals(northPos, "0 1 W");
+    }
+
     // endregion
 
 }
